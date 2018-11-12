e82da695f1105d439366faadcf9fa3cba8c4a2f6
zxs1995 <583608193@qq.com>
2018-11-12 09:46:21 +0800

    222

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class DemoController {
+    public void tet1(){
+        System.out.println("the master line");
+    }
+}
